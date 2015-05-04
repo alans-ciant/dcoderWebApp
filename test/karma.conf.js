2ed38b4696cb478237bdc50a39a1be6ed0c675f4
patrinhani-ciandt <patrinhani@ciandt.com>
2015-05-04 11:58:34 -0300

    Upload project

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,16 +23,6 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/eventEmitter/EventEmitter.js',
-      'bower_components/eventie/eventie.js',
-      'bower_components/imagesloaded/imagesloaded.js',
-      'bower_components/get-style-property/get-style-property.js',
-      'bower_components/get-size/get-size.js',
-      'bower_components/doc-ready/doc-ready.js',
-      'bower_components/matches-selector/matches-selector.js',
-      'bower_components/fizzy-ui-utils/utils.js',
-      'bower_components/outlayer/outlayer.js',
-      'bower_components/masonry/masonry.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-route/angular-route.js',
